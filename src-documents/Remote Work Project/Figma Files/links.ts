28f4d16e6ff41050da2fd4997102388f8da5d858
SHUVOBROTO BISWAS <93680258+ShuvobrotoBiswas@users.noreply.github.com>
2022-08-31 10:39:18 +0600

    docs uploaded

---
+++ src-documents/Remote Work Project/Figma Files/links.ts
@@ -0,0 +1,18 @@
+// import { Component, OnInit } from '@angular/core';
+// import { OrderDetailsService } from 'src/app/services/order-details.service';
+
+// @Component({
+//   selector: 'app-home',
+//   templateUrl: './home.component.html',
+//   styleUrls: ['./home.component.scss']
+// })
+// export class HomeComponent implements OnInit {
+
+//   constructor(private service:OrderDetailsService) { }
+//   foodData:any;
+//   ngOnInit(): void {
+//     this.foodData = this.service.foodDetails;
+//   }
+// }
+
+// https://www.figma.com/file/jFgofADQRxrlhW0f3ooCZY/Remote-Work-App?node-id=0%3A1
